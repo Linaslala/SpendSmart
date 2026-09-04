62f01b7685778b8628b29ab5779c187a85cbc5f4
Lina Samuelsson <lina.sk.samuelsson@hotmail.com>
2026-09-04 16:22:42 +0200

    set up the DbContext

---
+++ SpendSmart/Models/SpendSmartDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SpendSmart.Models
+{
+    public class SpendSmartDbContext : DbContext
+    {
+        //property
+        public DbSet<Expense> Expenses { get; set; }
+
+        //konstruktor
+        public SpendSmartDbContext(DbContextOptions<SpendSmartDbContext> options)
+            : base(options)
+        {
+            
+        }
+    }
+}
